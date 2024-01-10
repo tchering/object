e44c0ae57f9dead60cbc7c0fbc61e6d735082994
tchering <sonam.sherpa.lama@gmail.com>
2024-01-10 14:27:07 +0100

    test to print from database in index.php

--- index.php
+++ index.php
@@ -4,4 +4,5 @@ include("./Service/extra.php"); //! here we have separated function file in anot
 spl_autoload_register('charger'); // this is to auto include the file. with this we dont need to use include each time like above.
 $myFct = new MyFct();
 $article = $myFct->findByIdTable('article',2);
+$article = $myFct->listTable('article');
 $myFct->printr($article);
