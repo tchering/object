f4c3c6a69bf6ae37cb9800f96c676a79a98d4488
tchering <sonam.sherpa.lama@gmail.com>
2024-01-11 15:53:17 +0100

    Update article list view layout and functionality

--- View/article/list.html.php
+++ View/article/list.html.php
@@ -1,14 +1,14 @@
-<div class="w90 auto">
+<div class="col-sm-12">
     <h1 class="titre center text-light">Liste Article</h1>
-    <table class="table w-100">
+    <table class="table w-auto col-sm-12">
         <thead id="thead_art">
             <tr class="bg-success">
-                <th>ID</th>
-                <th>CODE</th>
-                <th>DESIGNATION</th>
-                <th>PU</th>
-                <th>ACTION</th>
-        <tbody id="tbody_art">
+                <th class ="w10">ID</th>
+                <th class ="w10">CODE</th>
+                <th class ="w40">DESIGNATION</th>
+                <th class ="w10">PU</th>
+                <th class ="w30">ACTION</th>
+        <tbody id="tbody_art" class="m-sm-0 p-sm-0">
         </tbody>
         <tfoot id="tfoot_art">
             <tr class="bg-success">
@@ -22,25 +22,31 @@
 
 <script>
     let articles = <?= $articles ?>;
-    console.log(articles);
     //!function to show the page in table body
     function afficher(tableName) {
+
         let html = articles.map((article) => {
             return `
-                <tr>
+                <tr class="">
                     <td>${article.id} </td>
                     <td>${article.numArticle} </td>
                     <td>${article.designation} </td>
                     <td>${article.prixUnitaire}</td>
-                    <td>
+                    <td class="buttons gap-sm-2 d-grid d-md-flex justify-content-around">
                         <button class="btn btn-sm btn-success">Afficher</button>
                         <button class="btn btn-sm btn-danger">Supprimer</button>
                         <button class="btn btn-sm btn-primary">Modifier</button>
                     </td>
                 </tr>
             `
-        })
+        }).join('');
+        // The .join('') method in JavaScript is used to join all elements of an array into a string. The argument you pass to .join() determines what goes between the elements when they are joined. If you don't pass any argument, the default is a comma (,).
+        console.log(html);
         tbody_art.innerHTML = html;
+        //! to show total number of articles.
+        const nbre = articles.length;
+        nbre_art.innerHTML = `Nombre d'articles : ${nbre}`;
     };
+    //!calling the function afficher with param articles.
     afficher(articles);
 </script>
