411d265b08c375be502c4423cb35f39f090287b0
tchering <sonam.sherpa.lama@gmail.com>
2024-01-17 08:35:42 +0100

    Add instantiation of Client class in ClientManager findById method

--- Model/ClientManager.php
+++ Model/ClientManager.php
@@ -7,6 +7,10 @@ class ClientManager extends Manager{
     public function findById($id,$type="object"){
         $resultat = $this->findByIdTable('client',$id);
        if($type){
+        //!here when Client class is instantiated the $resultat is pass in its param. 
+        //!We know when class is instantiated construct is auto called therefore construct is Client is called.
+        //! And inside constructor the $data is empty array for now  but will be filled with the data of $resultat.
+        //! Once data is true or not empty the method to gettersetter initiate.
         $object = new Client($resultat);//!instance of Client class
         return $object;
        } else {
